f77f5669dfbf1f6dc6b4ab4170142a07a3f55eea
viswakggautham <viswakggautham@gmail.com>
2023-12-08 21:04:20 +0530

    commit

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
+import random
+
 from flask import Flask
 from flask_cors import CORS
 import mysql.connector as mysql
 con=mysql.connect(host='localhost', user='root',password='password',database='flight_game')
 cursor=con.cursor()
-'''
+sum = 0
+
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/location', methods=['GET'])
-def hello_name():
-    query = 'select * from airport;'
-    cursor.execute(query)
-    data = cursor.fetchall()
-    for i in data:
-        print(i[4], i[5])
+sum2 = 0
+sum3 = 1000
+@app.route('/gold', methods=['GET'])
+def gold():
+    global sum, sum2, sum3
+    gold = random.randint(1,10)
+    sum = sum + gold
+    used = random.randint(1,100)
+    sum2 = sum2 + used
+    sum3 = sum3 - used
+    d = {'gold': sum, 'new':gold, 'used':sum2, 'remaining':sum3}
+    return d
 
 if __name__ == '__main__':
-    app.run(debug=True)'''
-l=[]
-query = 'select * from airport;'
-cursor.execute(query)
-data = cursor.fetchall()
-for i in data:
-    d = {}
-    d['location'] = [i[4],i[5]]
-    l.append(d)
-print(l)
+    app.run(debug=True)
--- templates/js/main.js
+++ templates/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 /* 1. show map using Leaflet library. (L comes from the Leaflet library) */
-
+let value = 0
 const map = L.map('map', {tap: false});
 L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   maxZoom: 20,
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
-map.setView([60, 24], 7);
+map.setView([60, 24], 7.5);
 
 // global variables
 
@@ -23,29 +23,97 @@ async function getData(url) {
 }
 
 // function to update game status
+function updateStatus(status){
+  document.querySelector('#player-name').innerHTML = `${status.name}`
+  document.querySelector('#consumed').innerHTML = `${status.co2.consumed}`
+  document.querySelector('#budget').innerHTML = `${status.co2.budget}`
+
+}
 
 // function to show weather at selected airport
+function showWeather(airport){
+  document.querySelector('#airport-name').innerHTML = `:${airport.name}`
+  document.querySelector('#temprature').innerHTML = `${airport.weather.temp}°C`
+  document.querySelector('#wind-speed').innerHTML = `${airport.weather.wind.speed}m/s`
+}
 
 // function to check if any goals have been reached
 
 // function to update goal data and goal table in UI
+async function gold(status){
+  const response = await fetch('http://127.0.0.1:5000/gold');
+  const data = await response.json();
+  alert(data.new)
+
+
+  document.querySelector('#gold').innerHTML = data.gold;
+  if(data.gold>500){
+    window.location.href = "http://localhost:63342/SOftware2/templates/win.html";
+  }
+  document.querySelector('#consumed').innerHTML = `${data.used}`
+  document.querySelector('#budget').innerHTML = `${data.remaining}`
+  if(data.remaining<0){
+    window.location.href = "http://localhost:63342/SOftware2/templates/loose.html";
+  }
+
+
+}
 
 // function to check if game is over
+
 
 // function to set up game
 // this is the main function that creates the game and calls the other functions
 async function gameSetup() {
   try {
     const gameData = await getData('testdata/newgame.json');
-
+    updateStatus(gameData.status)
+    gold()
     console.log(gameData);
-    for(let airport of gameData.location){
-      const marker = L.marker([airport.latitude,airport.longitude]).addTo(map)
-      if(airport.active){
+    for (let airport of gameData.location) {
+      let myIcon = L.icon({
+        iconUrl: 'img/my-icon.png',
+        iconSize: [30, 40],
+        iconAnchor: [30, 40],
+        popupAnchor: [-15, -14],
+        shadowUrl: 'my-icon-shadow.png',
+        shadowSize: [0, 0],
+        shadowAnchor: [0, 0],
+      });
+      let myIcom = L.icon({
+        iconUrl: 'img/my-icom.png',
+        iconSize: [30, 40],
+        iconAnchor: [30, 40],
+        popupAnchor: [-15, -14],
+        shadowUrl: 'my-icon-shadow.png',
+        shadowSize: [0, 0],
+        shadowAnchor: [0, 0],
+      });
+      let marker
+
+      if (airport.active) {
+        showWeather(airport)
+        marker = L.marker([airport.latitude, airport.longitude],
+          {icon: myIcom}).addTo(map);
         marker.bindPopup(`You are here:<b>${airport.name}</b>`).openPopup();
+
+      } else {
+        marker = L.marker([airport.latitude, airport.longitude],
+          {icon: myIcon}).addTo(map);
+        const popupContent = document.createElement('div');
+        const h4 = document.createElement('h4');
+        h4.innerHTML = airport.name;
+        popupContent.append(h4);
+        const goButton = document.createElement('button');
+        goButton.classList.add('button');
+        goButton.innerHTML = 'Fly here!';
+        popupContent.append(goButton);
+        const p = document.createElement('p');
+        p.innerHTML = `Distance is ${airport.distance} km`;
+        popupContent.append(p);
+        marker.bindPopup(popupContent);
+
       }
-
-
     }
   } catch (error) {
     console.log(error.message);
